5d75de993eeeb99ebecdd35144620031f143a099
bowl23 <1467059904@qq.com>
2023-11-05 16:29:17 +0800

    finish

--- todoList/index.js
+++ todoList/index.js
@@ -2,16 +2,23 @@ const main = new Vue({
     el: '#main',
     data: {
         hello: 'hello',
-        list: [
-            { id: 1, thing: 'a' },
-            { id: 2, thing: 'b' },
-            { id: 3, thing: 'c' },
-            { id: 4, thing: 'd' },
-            { id: 5, thing: 'e' }
-        ]
+        list: [],
+        todoname: '',
+        length: 0,
     },
     methods: {
-
+        del(id) {
+            this.list = this.list.filter(item => item.id != id);
+        },
+        add() {
+            this.length++;
+            this.list.push({
+                id: this.length,
+                thing: this.todoname
+            });
+            this.todoname = '';
+        },
     },
+
 });
 console.log(main.hello);
